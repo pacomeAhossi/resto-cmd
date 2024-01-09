2b72bf0f4d5c9c4b9860449aba169eda96d64586
Pacome AHOSSI <pacomeahossi175@gmail.com>
2024-01-09 10:20:20 +0100

    code base for redux installation and redux architecture

---
+++ src/app/store.js
@@ -0,0 +1,90 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { PouletCroquant, SuperCremeux } from "../common/models";
+
+let state = {
+    value: null,
+    owner: {},
+    list: [
+        SuperCremeux,
+        PouletCroquant
+    ]
+};
+
+// const PRODUCT_LIST = {
+//     DoubleCantal,
+//     PouletCroquant,
+//     SuperCremeux
+
+// };
+
+function reducer(state, action){
+    switch (action.type) {
+        case "ADD_PRODUCT":
+            const listWithNewProduct = [...state.list, action.payload]
+            return {...state, list: listWithNewProduct}
+            
+        case "REMOVE_PRODUCT":
+            const list = state.list.filter((item, index) => index !== action.payload)
+            return {...state, list: list}
+        case "APPLY_VOUCHER":
+            const withVoucherList = state.list.map(
+                (item) => item.title === 'Super Crémeux' ? ({...item, price: action.payload.price}) : {...item})
+                return {...state, list: withVoucherList}
+        case "UPDATE_FIRSTNAME":
+            const owner = {...state.owner, firstName: action.payload}
+            return {...state, owner}
+        default:
+            return state;
+    }
+}
+
+export const store = configureStore({
+    preloadedState: state,
+    reducer,
+});
+
+// store.subscribe(() => {
+//     const state = store.getState();
+//     if(state.owner){
+//         document.querySelector(".header").textContent = `Le propriétaire du restaurent est ${state.owner.firstName}`;
+//     }
+//     if(state.list){
+//         document.getElementById("command").innerHTML = `<h2>Vous avez selectionnés les produits suivants :</h2>`;
+//         for(const item of state.list){
+//             const itemElement = document.createElement("div");
+//             itemElement.innerHTML = `
+//                 <div>
+//                     ${item.title} <span>${item.price}</span>
+//                     <button >remove</button>
+//                 </div>
+//             `;
+//             document.getElementById("command").appendChild(itemElement);
+//         }
+//     }
+// })
+
+// document.querySelectorAll(".orderButton").forEach((element) => {
+//     element.addEventListener("click", (event) => {
+//         const productId = event.target.dataset['id'];
+//         store.dispatch(
+//             {type: 'ADD_PRODUCT', payload: PRODUCT_LIST[productId]}
+//         )
+
+//     })
+// });
+
+// document.getElementById("voucher").addEventListener("click", (evt) => {
+//     store.dispatch(
+//         {type: 'APPLY_VOUCHER', payload: {price : 4.00}}
+//     )
+// })
+
+// document.getElementById("addForm").addEventListener("submit", (evt) =>{
+//     evt.preventDefault();
+//     const firstNameInput = evt.currentTarget.firstName;
+//     store.dispatch(
+//         {type: 'UPDATE_FIRSTNAME', payload: firstNameInput.value}
+//     )
+// })
+
+// dispatch({...state, company: {name: 'Burger du Pré'}})
---
+++ src/features/cart/Cart.js
@@ -0,0 +1,16 @@
+import { useStore } from "react-redux";
+
+export const Cart = () => {
+    const store = useStore();
+
+    return (
+        <div className="Selection">
+            <h1>Liste des produits sélectionnés</h1>
+            {
+                store.getState().list.map(
+                    (item, index) => <span key={index} className="SelectedProduct">{item.title} {item.price} £</span>
+                )
+            }
+        </div>
+    )
+} 
